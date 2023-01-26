039289e0fc770745f07a4166b3bcf41f4fc66b87
Anders Ravn <anders@adriansen.dk>
2023-01-26 16:54:25 +0100

    Add: Added error messages to database module

--- server/utilities/database.js
+++ server/utilities/database.js
@@ -4,13 +4,26 @@ class Database {
   #connection = null;
 
   async connect() {
-    this.#connection = await MongoClient.connect(process.env.MONGODB)
+    // Create database connection object
+    this.#connection = await MongoClient.connect(process.env.MONGODB);
+
+    if (!this.#connection) {
+      throw Error("Failed to connect to database");
+    }
+
+    // Return connection object
+    return this.#connection;
   }
 
   get() {
-    if (!this.#connection){
-      throw Error("No connection has been initialized. Call connect method first")
+    if (!this.#connection) {
+      throw Error(
+        "No connection has been initialized. Call connect method first"
+      );
     }
+
+    // Return connection object
+    return this.#connection;
   }
 }
 
